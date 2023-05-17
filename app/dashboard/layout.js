d93c88efd07f26bc81d3ea474e974f6617b691a4
anildalar <dollorinfotech@gmail.com>
2023-05-17 10:30:55 +0530

    new

---
+++ app/dashboard/layout.js
@@ -0,0 +1,5 @@
+export default function DashboardLayout({children}) {
+    return (
+      <div id="dashboarlayout">{children}</div>
+    );
+  }
---
+++ app/dashboard/page.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Page() {
+    console.log('Hello How are you');
+    return (
+        <div>Dashboard page</div>
+    )
+}
